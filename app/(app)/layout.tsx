2a626dbb1abb08f7005cae6e29a29c97036db259
Eduardo Vedovatto <ed.vedovatto@gmail.com>
2026-03-31 13:34:20 -0300

    Add touch feedback animations and smoother sheet transitions

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,7 +5,7 @@ import BottomNav from '@/components/BottomNav'
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-dvh">
-      <main className="flex-1 pb-20">{children}</main>
+      <main className="flex-1 pb-20 animate-in fade-in duration-200">{children}</main>
       <BottomNav />
     </div>
   )
